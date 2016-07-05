37d59f70c886e11e841d7f5f8b086212a46afa85
stanierv97 <stanier_villega@hotmail.es>
2016-07-05 00:24:21 -0500

    Diseño de la calculadora

---
+++ CalculadoraSV/app/src/main/java/com/facci/calculadorasv/MainActivitySV.java
@@ -0,0 +1,14 @@
+package com.facci.calculadorasv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivitySV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_sv);
+    }
+
+}
